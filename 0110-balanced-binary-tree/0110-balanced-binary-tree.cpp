40f04131b9b5071cc73af7ac8d97be614316329e
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-14 15:05:47 +0530

    Time: 75 ms (7.7%), Space: 21.4 MB (14.49%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -15,31 +15,41 @@ public:
         if(root==NULL)
         {
             return NULL;
-        }
-        int left = length(root->left);
-        int right = length(root->right);
+        } //to find the length
+        int left = length(root->left); //length of left subtree
+        int right = length(root->right); //length of right subtree
 
-        if(left==-1 || right==-1){
+        if(left==-1 || right==-1){ //if either of the subtree returns unbalanced then the whole tree is unbalanced.. so return -1
           return -1;
         }
 
-        if(abs(left-right)>1){
+        if(abs(left-right)>1){ //if the difference between left and right is >=1 then it is unbalanced
           return -1;
         }
-
-        return max(left,right)+1;
-
+        return max(left,right)+1; //for returning the length 
 
     }
 
     bool isBalanced(TreeNode* root) {
-      int c = length(root);
-      if(c==-1){
+      if(root==NULL){
+        return true;
+      }
+      int lefth = length(root->left);
+      int righth= length(root->right);
+
+      if(abs(lefth-righth)>1){
         return false;
       }
-      return true;
+      bool left = isBalanced(root->left);
+      bool right = isBalanced(root->right);
 
+      if(!left || !right){
+        return false;
+      }
+      else{
+        return true;
+      }
 
-        
+      
     }
 };
