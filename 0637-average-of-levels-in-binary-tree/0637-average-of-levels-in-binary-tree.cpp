0a1f6a9c86f7cff8e1cd0f1e97c3c8847615cb06
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-16 16:43:44 +0530

    Time: 7 ms (92.97%), Space: 22.9 MB (11.21%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -12,32 +12,36 @@
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> sum;
-        vector<int> count;
-        vector<double> averages;
+        vector<double> sum;   // Vector to store the sum of values for each level.
+        vector<int> count;    // Vector to store the count of nodes for each level.
+        vector<double> averages; // Vector to store the calculated average values.
 
+        // Start DFS traversal from the root node at level 0.
         dfs(root, 0, sum, count);
 
+        // Calculate the averages for each level.
         for (int i = 0; i < sum.size(); i++) {
-            averages.push_back(sum[i] / count[i]);
+            averages.push_back(sum[i] / count[i]); // Calculate average as sum/count.
         }
 
-        return averages;
+        return averages; // Return the vector of average values.
     }
 
     void dfs(TreeNode* node, int level, vector<double>& sum, vector<int>& count) {
         if (!node) {
-            return;
+            return; // Base case: If the current node is null, return.
         }
 
+        // Check if the current level is greater than or equal to the size of sum and count vectors.
         if (level >= sum.size()) {
-            sum.push_back(0);
-            count.push_back(0);
+            sum.push_back(0);   // Initialize sum for the new level as 0.
+            count.push_back(0); // Initialize count for the new level as 0.
         }
 
-        sum[level] += node->val;
-        count[level]++;
+        sum[level] += node->val; // Add the value of the current node to the sum of the current level.
+        count[level]++; // Increment the count of nodes at the current level.
 
+        // Recursively call dfs for left and right children with an incremented level.
         dfs(node->left, level + 1, sum, count);
         dfs(node->right, level + 1, sum, count);
     }
