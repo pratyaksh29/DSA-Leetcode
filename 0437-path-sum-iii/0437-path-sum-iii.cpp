c9dd802055bd3bc836f553ef47df4b738c608b96
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-17 15:58:13 +0530

    Time: 11 ms (94.17%), Space: 19.6 MB (34.18%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int ans=0;
-    int pathSum(TreeNode* root, int sum) {
-        if(root){
-            dfs(root,sum);
-            pathSum(root->left,sum);
-            pathSum(root->right,sum);
-        }
-        return ans;
+    unordered_map<long, int> map;
+    int count = 0;
+    
+    void countPathSum(TreeNode* root, int target, long sum){
+        if(!root)
+            return;
+        sum += root->val;        //Path sum from root
+        if(sum == target)
+            count++;
+        if(map.find(sum - target) != map.end())         //checking whether any target sum path present in the path from root to the current node
+            count += map[sum - target];
+        map[sum]++;
+        countPathSum(root->left, target, sum);
+        countPathSum(root->right, target, sum);
+        map[sum]--;      //After visiting the left and right subtree, we have to reduce this path sum count from map since we are leaving this path
     }
-    void dfs(TreeNode* root, long long sum){
-        if(!root)return;
-        if(root->val==sum)ans++;
-        dfs(root->left,sum-root->val);
-        dfs(root->right,sum-root->val);
+    
+    int pathSum(TreeNode* root, int targetSum) {
+        countPathSum(root, targetSum, 0);
+        return count;
     }
 };
