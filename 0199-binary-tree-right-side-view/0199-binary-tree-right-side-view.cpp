ba2fac4fa7da3a7df9c3604f05a886b644baf894
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-17 22:48:42 +0530

    Time: 3 ms (62.24%), Space: 12.2 MB (21.69%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -13,28 +13,19 @@ class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
-        if(!root){
-            return ans;
-        }
-        queue<TreeNode*> q;
-        q.push(root);
+        dfs(root,1,ans);
+        return ans;
+        
+    }
 
-        while(!q.empty()){
-            int size = q.size();
-            vector<int>level;
-            for(int i =0;i<size;i++){
-                TreeNode* node = q.front();
-                level.push_back(node->val);
-                q.pop();
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-            ans.push_back(level[level.size()-1]);
-        }   
-        return ans;     
+    void dfs(TreeNode* root, int level, vector<int> &ans){
+        if(!root){
+            return;
+        }
+        if(ans.size()<level){
+            ans.push_back(root->val);
+        }
+        dfs(root->right,level+1,ans);
+        dfs(root->left,level+1,ans);
     }
 };
