07f4dd33d1e43e4b567395935083bf3d39f75f27
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-18 03:35:11 +0530

    Time: 105 ms (55.75%), Space: 86.6 MB (6.97%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count =0;
+    int goodNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        countgoodnodes(root,root->val);
+        return count;
+        
+    }
+
+    void countgoodnodes(TreeNode* root,int max){
+        if(root==NULL){
+            return;
+        }
+        if(root->val>=max){
+            count++;
+            max=root->val;
+        }
+        countgoodnodes(root->left, max);
+        countgoodnodes(root->right, max);
+
+    }
+};
