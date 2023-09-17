82a384f649ea3b48c79a739103ee1d4b78148cba
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-17 17:41:02 +0530

    Time: 8 ms (97.67%), Space: 19.6 MB (34.36%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,24 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<long, int> map;
     int count = 0;
-    
-    void countPathSum(TreeNode* root, int target, long sum){
-        if(!root)
+    unordered_map<long,long>mp; //sum,count
+
+    void pathcalc(TreeNode* root, int targetSum, long runningSum){ 
+        if(root==NULL){
             return;
-        sum += root->val;        //Path sum from root
-        if(sum == target)
+        }
+        runningSum += root->val;
+        if(runningSum==targetSum){
             count++;
-        if(map.find(sum - target) != map.end())         //checking whether any target sum path present in the path from root to the current node
-            count += map[sum - target];
-        map[sum]++;
-        countPathSum(root->left, target, sum);
-        countPathSum(root->right, target, sum);
-        map[sum]--;      //After visiting the left and right subtree, we have to reduce this path sum count from map since we are leaving this path
+        }
+        if(mp.find(runningSum-targetSum)!=mp.end()){
+            count+=mp[runningSum-targetSum]; //got stuck here
+        }
+        mp[runningSum]++;
+        pathcalc(root->right,targetSum,runningSum);
+        pathcalc(root->left,targetSum,runningSum);
+        mp[runningSum]--;
     }
-    
+
     int pathSum(TreeNode* root, int targetSum) {
-        countPathSum(root, targetSum, 0);
+        pathcalc(root,targetSum,0);
         return count;
     }
 };
