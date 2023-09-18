6292523d461e5cbb5f71d18a110298e3329c1e3e
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-09-18 14:02:11 +0530

    Time: 94 ms (88.77%), Space: 86.9 MB (5.19%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -15,21 +15,22 @@ public:
         if(!root){
             return 0;
         }
-        stack<pair<TreeNode*,int>>st;
+        stack<pair<TreeNode*,int>>st; //create a pair in a stack to store the node and the max value till that node
         st.push({root,root->val});
         int goodnodes = 0;
 
         while(st.empty()==false){
             auto [node,maxval] = st.top();
             st.pop();
-            if(node->val>=maxval){
+            if(node->val>=maxval){ //if
                 goodnodes++;
+                maxval = max(maxval,node->val);
             }
             if(node->left){
-                st.push({node->left,max(maxval,node->val)});
+                st.push({node->left,maxval});
             }
             if(node->right){
-                st.push({node->right,max(maxval,node->val)});
+                st.push({node->right,maxval});
             }
         }
         return goodnodes;
