2dc0445a7e2997f218ec0e2d7e8c868341fc6eec
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-10-01 13:59:59 +0530

    Time: 9 ms (21.5%), Space: 13.4 MB (32.1%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,59 +1,70 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    vector<int>ans;
-    unordered_map<TreeNode* ,TreeNode*>childtoparent; //child,parent
+    vector<int> ans;
+    unordered_map<TreeNode*, TreeNode*> childToParent;
     unordered_set<TreeNode*> visited;
 
-    void findParent(TreeNode* node){
-        if(!node) return;
-        if(node->left){
-            childtoparent[node->left]=node;
-            findParent(node->left);
-        }
-        if(node->right){
-            childtoparent[node->right]=node;
-            findParent(node->right);
-        }
-    }
-
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if(!root){
-            return {};
+        // Build the child to parent mapping
+        buildChildToParent(root);
+
+        queue<TreeNode*> q;
+        q.push(target);
+        visited.insert(target);
+
+        int currentLevel = 0;
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            if (currentLevel == k) {
+                while (!q.empty()) {
+                    ans.push_back(q.front()->val);
+                    q.pop();
+                }
+                return ans;
+            }
+
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left && visited.find(node->left) == visited.end()) {
+                    q.push(node->left);
+                    visited.insert(node->left);
+                }
+                if (node->right && visited.find(node->right) == visited.end()) {
+                    q.push(node->right);
+                    visited.insert(node->right);
+                }
+                TreeNode* parent = childToParent[node];
+                if (parent && visited.find(parent) == visited.end()) {
+                    q.push(parent);
+                    visited.insert(parent);
+                }
+            }
+            currentLevel++;
         }
-        findParent(root);
-        dfs(target,k);
+
         return ans;
     }
 
-    void dfs(TreeNode* target, int k){
-        if(visited.find(target)!=visited.end()){
-            return;
-        }
-        visited.insert(target);
-        if(k==0){
-            ans.push_back(target->val);
-            return;
-        }
-        if(target->left){
-            dfs(target->left,k-1);
-        }
-        if(target->right){
-            dfs(target->right,k-1);
-        }
-        TreeNode* p = childtoparent[target];
-        if(p){
-            dfs(p,k-1);
-        }
+    void buildChildToParent(TreeNode* root) {
+        stack<TreeNode*> s;
+        s.push(root);
+        childToParent[root] = nullptr;
 
+        while (!s.empty()) {
+            TreeNode* node = s.top();
+            s.pop();
 
+            if (node->left) {
+                s.push(node->left);
+                childToParent[node->left] = node;
+            }
+            if (node->right) {
+                s.push(node->right);
+                childToParent[node->right] = node;
+            }
+        }
     }
 };
