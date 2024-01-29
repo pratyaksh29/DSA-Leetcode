b551c24ba70ec4e2dece6083fb0b312960f353da
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-01-29 14:53:37 +0530

    Time: 13 ms (64.39%) | Memory: 19.7 MB (22.60%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,32 +1,48 @@
 class Solution {
-private:
-    int f(int ind,int target,vector<int>& coins,vector<vector<int>>&dp){
-        if(ind==0){
-            if(target%coins[0]==0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        if(dp[ind][target]!=-1){
-            return dp[ind][target];
-        }
-        int dontpick = f(ind-1,target,coins,dp);
-        int pick = 0;
-        if(coins[ind]<=target){
-            pick = f(ind,target - coins[ind],coins,dp);
-        }
-        return dp[ind][target] = dontpick +pick;
+// private: //memoization
+//     int f(int ind,int amount,vector<int>& coins,vector<vector<int>>&dp){
+//         if(ind==0){
+//             if(amount%coins[0]==0){
+//                 return 1; //messed up here
+//             }
+//             else{
+//                 return 0;
+//             }
+//         }
+//         if(dp[ind][amount]!=-1){
+//             return dp[ind][amount];
+//         }
+//         int dontpick = f(ind-1,amount,coins,dp);
+//         int pick = 0;
+//         if(coins[ind]<=amount){
+//             pick = f(ind,amount - coins[ind],coins,dp); //messed up here 
+//         }
+// //basically we wont have to write 1 + f() for pick because we arent counting the number of coins needed to reach the amount
+// //we are counting the number of ways in which the amount can be reached -> that is why we return 1 in the base case instead of amount/coins[0]
+//         return dp[ind][amount] = dontpick +pick;
 
-    }
-
-
+//     }
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
-        
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+
+
+        for (int target = 0; target <= amount; target++) {
+            dp[0][target] = (int) target % coins[0] == 0; 
+        }
+
+        for(int i =1;i<n;i++){
+            for(int t = 0;t<=amount;t++){
+                int dontpick = dp[i-1][t];
+                int pick = 0;
+                if(coins[i]<=t){
+                    pick = dp[i][t-coins[i]];
+                }
+                dp[i][t]=pick+dontpick;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 };
