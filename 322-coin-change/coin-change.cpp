f7e7e7e25714d0754e1321a21fc7c3e501f27032
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-01-29 13:19:04 +0530

    Time: 37 ms (89.20%) | Memory: 20.2 MB (35.33%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -21,34 +21,32 @@ private:
 
 // //infinite supply, multiple use, etc -> whenever u take -> stand at same index
 //     }
-public:
+public: //tabulation
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int>base(amount+1,0),curr(amount+1,0);
 
-        for(int i =0;i<=amount;i++){
+        for(int i =0;i<=amount;i++){ //stuck here
             if(i%coins[0]==0){
-                dp[0][i]= i/coins[0];
+                base[i]= i/coins[0];
             }
             else{
-                dp[0][i]=1e9;
+                base[i]=1e9;
             }
         }
-
         for(int i =1;i<n;i++){
             for(int j=0;j<=amount;j++){
-                int dontpick = 0 + dp[i-1][j];
+                int dontpick = 0 + base[j];
                 int pick = 1e9;
                 if(coins[i]<=j){
-                    pick = 1+ dp[i][j-coins[i]];
+                    pick = 1+ curr[j-coins[i]];
 
                 }
-                dp[i][j]=min(pick,dontpick);
+                curr[j]=min(pick,dontpick);
             }
+            base=curr;
         }
-        int ans = dp[n-1][amount];
-
-
+        int ans = base[amount];
         if(ans>=1e9){
             return -1;
         }
