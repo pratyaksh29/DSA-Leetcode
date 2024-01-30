e9efa304636f8a2ecc3369ae6c59c37131cd169c
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-01-30 23:07:07 +0530

    Time: 62 ms (45.92%) | Memory: 29.9 MB (7.07%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    int f(int ind1, int ind2, string &text1, string &text2,vector<vector<int>> &dp){//pass by reference as everytime u call the string, it might give tle
+        if(ind1<0 || ind2<0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+        if(text1[ind1]==text2[ind2]){
+            return dp[ind1][ind2] = 1 + f(ind1-1, ind2-1, text1,text2,dp);
+        }
+
+        return dp[ind1][ind2] = max(f(ind1-1, ind2,text1,text2,dp),f(ind1, ind2-1,text1,text2,dp));
+    } 
+
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        string t =s; //just use the reverse of the string 1 as string 2 to get longest palindromic sequence
+        reverse(t.begin(),t.end());
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return f(n-1,n-1,s,t,dp);
+    }
+
+public:
+    int minInsertions(string s) {
+        return s.size()-longestPalindromeSubseq(s);
+    }
+};
