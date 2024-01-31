9e460a919edfea9fe5c92cdb9c81704d43927de1
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-01-31 15:31:54 +0530

    Time: 28 ms (62.83%) | Memory: 19.6 MB (49.14%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int f(int i,int j , string &s, string &t,vector<vector<int>>&dp){
+
+        if(j<0){ //if t is exhausted -> it means that we made all subsequences of s that could be made (cuz it only decrements when it matches ; thus we can return 1)
+            return 1;
+        }
+        if(i<0){
+            return 0; //if s is exhausted this wud mean that t still has letters left and thus we werent able to form subsequences in that recursive call
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(s[i]==t[j]){
+            //pick the one thats matching + dont pick and go to the other letters to find if there are more matching
+            return dp[i][j] = f(i-1,j-1,s,t,dp) + f(i-1,j,s,t,dp);
+        }
+        else{
+            return dp[i][j]= f(i-1,j,s,t,dp);
+        }
+    }
+public:
+    int numDistinct(string s, string t) {
+        
+        int n = s.size();
+        int m =t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n-1,m-1,s,t,dp);
+    }
+};
+
