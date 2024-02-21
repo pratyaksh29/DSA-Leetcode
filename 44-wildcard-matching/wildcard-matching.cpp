fcf06eb5170389273b6bf172ad4483532e5a98dd
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-02-21 13:56:32 +0530

    Time: 35 ms (71.01%) | Memory: 31.7 MB (8.13%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -3,25 +3,31 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
         dp[0][0] = 1;
 
-        for (int j = 1; j <= m; j++) {
-            if (p[j - 1] == '*') { // use j-1 instead of j
-                dp[0][j] = dp[0][j - 1];
-            } else {
-                dp[0][j] = 0;
+        for(int j =1;j<=m;j++){
+            bool flag = true;
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    flag = false;
+                    break;
+                }
             }
+            dp[0][j] = flag;
+            
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') { // use i-1 and j-1 instead of i and j
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j - 1] == '*') { // use j-1 instead of j
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1]; // use logical OR instead of +
-                } else {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){ 
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
                     dp[i][j] = 0;
                 }
             }
