89b5cd1d050e56fc045013d91e5e42d302186f97
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-02-21 14:08:03 +0530

    Time: 38 ms (64.69%) | Memory: 31.6 MB (8.13%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,36 +1,38 @@
 class Solution {
+private:
+    bool f(string &s,string &p,int i,int j,vector<vector<int>>&dp){ //i -> string size ; //j -> pattern size
+        if(i<0 && j<0){         //base case; if both are exhausted then this means that the pattern matched with the string
+            return true;
+        }
+        if(i>=0 && j<0){        //if the pattern has exhausted but the string is still there 
+            return false;      //then return false because there is no way that the string can match with the pattern
+        }
+        if(i<0 && j>=0){ //if the string has exhausted ; the pattern can still match with it if and only if it has all '*' in the string
+            for(int k=0;k<=j;k++){
+                if(p[k]!='*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];        
+        }
+        if(s[i]==p[j]||p[j]=='?'){ //if it matches or the pattern has a ? ; we will just go back one place in both the string and pattern
+            return dp[i][j] = f(s,p,i-1,j-1,dp);
+        }
+        if(p[j]=='*'){ //if it has a * then there can be 2 options -> one we stay at the current string and go to the next pattern (dont use the power of *)
+            return dp[i][j] = f(s,p,i,j-1,dp) + f(s,p,i-1,j,dp);//-> other is that we stay at the current pattern (use power of *) and go to the next string letter
+        }
+        return false;  
+    }
 public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+
+        return f(s,p,n-1,m-1,dp);
         
-        dp[0][0] = 1;
-
-        for(int j =1;j<=m;j++){
-            bool flag = true;
-            for(int k=1;k<=j;k++){
-                if(p[k-1]!='*'){
-                    flag = false;
-                }
-            }
-            dp[0][j] = flag;
-            
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else if(p[j-1]=='*'){ 
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                }
-                else{
-                    dp[i][j] = 0;
-                }
-            }
-        }
-        return dp[n][m];
     }
 };
