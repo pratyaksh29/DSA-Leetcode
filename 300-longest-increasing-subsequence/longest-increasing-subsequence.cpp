803c755ffc5fdb6401e5249324a6152ea5255ef1
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-02-22 21:52:52 +0530

    Time: 151 ms (58.62%) | Memory: 13.1 MB (24.93%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,41 @@
 class Solution {
-private:
-    int f(int ind,int prev, vector<int>& nums, int n,vector<vector<int>>&dp){
-        if(ind == n){
-            return 0;
-        }
-        if(dp[ind][prev+1]!=-1){
-            return dp[ind][prev+1];
-        }
-        int len = 0 + f(ind+1,prev,nums,n,dp);
-        if(prev == -1 || nums[ind]>nums[prev]){
-            len = max(len,1+ f(ind+1,ind,nums,n,dp));
-        }
+// private:
+//     int f(int ind,int prev, vector<int>& nums, int n,vector<vector<int>>&dp){
+//         if(ind == 0){
+//             return 0;
+//         }
+//         if(dp[ind][prev+1]!=-1){ //we will do prev+1 because prev goes from -1 to n-1 
+//             return dp[ind][prev+1]; //so we need to change the index to prev +1 so its in bounds
+//         }
+//         int len = 0 + f(ind+1,prev,nums,n,dp); //not take
 
-        return dp[ind][prev+1]=len;
-    }
-public:
+//         if(prev == -1 || nums[ind]>nums[prev]){ //if its the first element or if the sequence is increasing
+//             len = max(len,1+ f(ind+1,ind,nums,n,dp));
+//         }
+
+//         return dp[ind][prev+1]=len;
+//     }
+public: //space optimization
     int lengthOfLIS(vector<int>& nums) {
         int n =nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return f(0,-1,nums,n,dp);
+        vector<int>after(n+1,0);//index & previous index
+        vector<int>cur(n+1,0);
+
+        for(int ind=n-1;ind>=0;ind--){ //go till 0 as that was the value in memo
+            for(int prev = ind-1;prev>=-1;prev--){ //go till -1 as that was the value in memo
+            //add prev+1 at every place where we have to update the dp as we are going to increase the prev value in the dp
+
+                int len = 0 + after[prev+1]; //not take 
+
+                if(prev == -1 || nums[ind]>nums[prev]){ //if its the first element or if the sequence is increasing
+                    len = max(len,1+ after[ind+1]);
+                }
+                cur[prev+1]=len;
+            }
+            after = cur;
+        }
+
+        return after[0];
     }
 };
+
