a52e922850bbf3a4cfd0507d8fbabd6db8ff7e4f
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-02-22 16:51:02 +0530

    Time: 8 ms (72.77%) | Memory: 15.9 MB (33.87%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,30 +1,40 @@
 class Solution {
-private:
-    int f(int ind, int transno,int k,int n, vector<int>&prices,vector<vector<int>>&dp){
-        if(ind == n || transno==k*2){
-            return 0;
-        }
-
-        if(dp[ind][transno]!=-1){
-            return dp[ind][transno];
-        }
-
-        //buy
-        if(transno%2==0){
-            return dp[ind][transno] = max((-prices[ind]+f(ind+1,transno+1,k,n,prices,dp)),f(ind+1,transno,k,n,prices,dp));
-        }
-        //sell
-        else{
-            return dp[ind][transno] = max((prices[ind]+f(ind+1,transno+1,k,n,prices,dp)),f(ind+1,transno,k,n,prices,dp));
-        }
-
-    } 
-
-
+// private:
+//     int f(int ind, int transno,int k,int n, vector<int>&prices,vector<vector<int>>&dp){
+//         if(ind == n || transno==k*2){ //k*2 because we one transaction is equal to buy + sell ; we want to check each buy and sell's index number                             
+//             return 0;                 //because if it is even then it would mean buy and if it is odd then it would mean sell
+//         }
+//         if(dp[ind][transno]!=-1){
+//             return dp[ind][transno];
+//         }
+//         //buy
+//         if(transno%2==0){
+//             return dp[ind][transno] = max((-prices[ind]+f(ind+1,transno+1,k,n,prices,dp)),f(ind+1,transno,k,n,prices,dp));
+//         }
+//         //sell
+//         else{
+//             return dp[ind][transno] = max((prices[ind]+f(ind+1,transno+1,k,n,prices,dp)),f(ind+1,transno,k,n,prices,dp)); 
+//         }//increase transno when u buy or sell otherwise keep it the same
+//     } 
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(k*2,-1));
-        return f(0,0,k,n,prices,dp);
+        vector<vector<int>>dp(n+1,vector<int>((k*2)+1,0)); //make a dp array for index and transaction number
+
+
+        for(int ind =n-1;ind>=0;ind--){ //make sure u go backwards
+            for(int transno=0;transno < (k*2);transno++){
+                if(transno%2==0){
+                    dp[ind][transno] = max((-prices[ind]+dp[ind+1][transno+1]),dp[ind+1][transno]);
+                }
+                //sell
+                else{
+                    dp[ind][transno] = max((prices[ind]+dp[ind+1][transno+1]),dp[ind+1][transno]);
+                }
+
+            }
+        }
+        return dp[0][0];
     }
 };
+
