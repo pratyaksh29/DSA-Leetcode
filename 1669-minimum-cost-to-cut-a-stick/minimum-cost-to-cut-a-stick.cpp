2f71b3e4ffa441ff12814c1b8b393919e5fdd3ab
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-02-28 15:47:26 +0530

    Time: 24 ms (86.67%) | Memory: 12.2 MB (46.37%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -8,8 +8,9 @@ private:
             return dp[i][j];
         }
         int mini= 1e9;
-        for(int ind =i;ind<=j;ind++){
-            int cost = cuts[j+1]-cuts[i-1] + f(i,ind-1,cuts,dp) + f(ind+1,j,cuts,dp);
+        for(int ind =i;ind<=j;ind++){ //go till j
+            int cost = cuts[j+1]-cuts[i-1] + f(i,ind-1,cuts,dp) + f(ind+1,j,cuts,dp); //compute the length with is cuts[j+1]-cuts[i-1]
+                                                                                     //compute the left and right sides of the cuts
             if(cost<mini){
                 mini=cost;
             }
@@ -18,14 +19,28 @@ private:
     }
 public:
     int minCost(int n, vector<int>& cuts) {
-        int j = cuts.size();
-        cuts.push_back(n);
-        cuts.push_back(0);
+        int c = cuts.size();
+
+        cuts.push_back(n); //push the last element
+        cuts.push_back(0); //push the first element and sort it
         sort(cuts.begin(),cuts.end());
         
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0)); //make the dp array with c because thats the total number of cuts
 
-        vector<vector<int>>dp(j+1,vector<int>(j+1,-1));
-
-        return f(1,j,cuts,dp);
+        
+        for(int i=c;i>=1;i--){
+            for (int j = 1; j <= c; j++) {
+                if (i > j) continue;
+                int mini = INT_MAX;
+                for (int ind = i; ind <= j; ind++) {
+                // Calculate the cost for making a cut at position 'ind'.
+                    int ans = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                    mini = min(mini, ans);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][c];
     }
 };
+
