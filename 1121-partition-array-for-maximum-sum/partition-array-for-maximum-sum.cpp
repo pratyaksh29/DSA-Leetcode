03fe079f8921c8aa9ed82ccdb3b1e20a512f0513
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-02 03:56:38 +0530

    Time: 4 ms (89.18%) | Memory: 10.8 MB (67.76%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -11,17 +11,31 @@ class Solution {
 
         int maxi=-1e9;
         int ans=0;
-        for(int j=i;j<(min(n,k+i));j++){ 
-            maxi = max(maxi,arr[j]);
-            int sum = maxi * (j-i+1) + f(j+1,k,arr,dp);
-            ans = max(ans,sum);
-        }
+
+        for(int j=i;j<(min(n,k+i));j++){ //j will run till k limit + i or to n ; whichever exhausts first -> stuck here
+            maxi = max(maxi,arr[j]); //we will store the maximum element from array
+            int sum = maxi * (j-i+1) + f(j+1,k,arr,dp); //we will add that to the sum and multiple the number by the length ; 
+            ans = max(ans,sum);                         //then compute rest of the array after making partition
+        } //store the max sum
         return dp[i] = ans;
     }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int>dp(n+1,-1);
-        return f(0,k,arr,dp);        
+        vector<int>dp(n+1,0);
+        dp[n]=0;
+        for(int i =n-1;i>=0;i--){
+            int maxi=-1e9;
+            int ans=0;
+            for(int j=i;j<(min(n,k+i));j++){ //j will run till k limit + i or to n ; whichever exhausts first -> stuck here
+                maxi = max(maxi,arr[j]); //we will store the maximum element from array
+                int sum = maxi * (j-i+1) + dp[j+1]; //we will add that to the sum and multiple the number by the length ; 
+                ans = max(ans,sum);                         //then compute rest of the array after making partition
+            } //store the max sum
+            dp[i] = ans;
+
+        }
+        return dp[0];   
     }
 };
+
