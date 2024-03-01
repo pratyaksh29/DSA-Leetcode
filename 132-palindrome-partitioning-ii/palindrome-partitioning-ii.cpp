7e3b453d58fa73e88efe8e801b5c9269723d77bb
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-02 03:10:03 +0530

    Time: 1755 ms (16.87%) | Memory: 7.7 MB (68.46%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 private:
-    bool isPalindrome(string &s){
-        int i = 0;
-        int j = s.size()-1;
+    bool isPalindrome(int i,int j,string &s){ //check for palindrome
         while(i<=j){
             if(s[i]!=s[j]){
                 return false;
@@ -16,29 +14,33 @@ private:
     }
 
     int f(int i,int n,string &s,vector<int>&dp){
-        if(i==n){
+        if(i==n){ //if i crosses the size of the string then return 0
             return 0;
         }
+
         if(dp[i]!=-1){
             return dp[i];
         }
-        string temp = "";
+
         int mini=1e9;
-        for(int j=i;j<n;j++){
-            temp+=s[j];
-            if(isPalindrome(temp)){
-                int cost = 1 + f(j+1,n,s,dp);
-                mini = min(cost,mini);
+
+        for(int j=i;j<n;j++){ //check from j=i to the entire string
+            if(isPalindrome(i,j,s)){ //check for palindrome  -> we can just use the index because then it will only check the string in that index range
+                int cost = 1 + f(j+1,n,s,dp); //if palindrome -> add cost as 1 and then do a partition and check from the next index
+                mini = min(cost,mini); //store minimum cost
             }
         }
         return dp[i] = mini;
     }
+
 public:
     int minCut(string s) {
         int n = s.size();
         vector<int>dp(n+1,-1);
-        return f(0,n,s,dp)-1;
-
-        
+        return f(0,n,s,dp)-1; //do -1 in the end because they end up doing an extra partition in the end which doesnt have to be accounted for
     }
 };
+
+
+
+
