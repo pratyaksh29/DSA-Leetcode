7faac56d0afb625618e2239245c73ff788eabfeb
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-12 16:40:42 +0530

    Time: 550 ms (23.98%) | Memory: 235.7 MB (31.63%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 
-void merge(vector<int> &arr, int low, int mid, int high) {
+void merge(vector<int> &arr, int low, int mid, int high) { //no changes
     vector<int> temp; // temporary array
     int left = low;      // starting index of left half of arr
     int right = mid + 1;   // starting index of right half of arr
@@ -46,8 +46,8 @@ int countPairs(vector<int> &arr, int low, int mid, int high){
         while(j<=high && arr[i]> 2LL * arr[j]){
             j++;
         }
-        c+=(j-(mid+1));
-    }
+        c+=(j-(mid+1)); //we dont do another +1 because j gets incremented in the end ; 
+    }                   //so it points to the number that is not to be added in the count
     return c;
 }
 
@@ -58,7 +58,7 @@ int mergeSort(vector<int> &arr, int low, int high) {
     int mid = (low + high) / 2 ;
     c += mergeSort(arr, low, mid);  // left half
     c+= mergeSort(arr, mid + 1, high); // right half
-    c+= countPairs(arr,low,mid,high);
+    c+= countPairs(arr,low,mid,high); //add the count pair function here and add all to the total count
     merge(arr, low, mid, high);  // merging sorted halves
     return c;
 }
