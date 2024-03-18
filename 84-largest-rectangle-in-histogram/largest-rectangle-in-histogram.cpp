d32668e2399781def7c21f1ff31d90df94659852
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-18 15:00:49 +0530

    Time: 150 ms (12.66%) | Memory: 94.7 MB (12.97%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
-    vector<int> nextSmallerElementIndex(vector<int>& heights,int n){
-        stack<int> st;
-        vector<int> res(n);
-        st.push(-1);
-        for(int i=n-1;i>=0;i--){
-            int curr=heights[i];
-            while(st.top()!=-1 && heights[st.top()]>=curr){
+    vector<int> nsr(vector<int>& heights) {
+        vector<int> ans;
+        int n = heights.size();
+        stack<pair<int, int>> st; // height, index
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top().first >= heights[i]) {
                 st.pop();
             }
-            res[i]=st.top();
-            st.push(i);
+            if (!st.empty() && st.top().first < heights[i]) {
+                ans.push_back(st.top().second); // Corrected calculation
+            } else {
+                ans.push_back(n); // Assume a virtual bar of height 0 at the end
+            }
+            st.push({heights[i], i});
         }
-        return res;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
-    
-    vector<int> prevSmallerElementIndex(vector<int>& heights,int n){
-        stack<int> st;
-        vector<int> res(n);
-        st.push(-1);
-        for(int i=0;i<n;i++){
-            int curr=heights[i];
-            while(st.top()!=-1 && heights[st.top()]>=curr){
+
+    vector<int> nsl(vector<int>& heights) {
+        vector<int> ans;
+        int n = heights.size();
+        stack<pair<int, int>> st; // height, index
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && st.top().first >= heights[i]) {
                 st.pop();
             }
-            res[i]=st.top();
-            st.push(i);
-        }
-        return res;
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        //Storing next smaller element index
-        vector<int> next=nextSmallerElementIndex(heights,n);
-        //Storing prev smaller element index
-        vector<int> prev=prevSmallerElementIndex(heights,n);
-        
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            int l=heights[i];
-            if(next[i]==-1) next[i]=n;
-            int b=next[i]-prev[i]-1;
-            ans=max(ans,l*b);
+            if (st.empty()) {
+                ans.push_back(-1); // Corrected condition
+            } else {
+                ans.push_back(st.top().second);
+            }
+            st.push({heights[i], i});
         }
         return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxi = INT_MIN;
+        vector<int> nsr_indices = nsr(heights); //call here not in the function
+        vector<int> nsl_indices = nsl(heights);
+        for (int i = 0; i < n; i++) {
+            int ind = nsr_indices[i] - nsl_indices[i] - 1; //stuck here
+            maxi = max(maxi, heights[i] * ind);
+        }
+        return maxi;
     }
 };
