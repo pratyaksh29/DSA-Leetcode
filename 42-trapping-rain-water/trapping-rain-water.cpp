c483dc4571f2cd6d2c763d07c4a6f9e95b938860
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-20 16:18:15 +0530

    Time: 3 ms (99.12%) | Memory: 22.3 MB (80.46%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    vector<int> left(vector<int>& height){ //max 
-        int n = height.size();
-        vector<int>res(n);
-
-        int maxi = height[0]; //stuck here
-        res[0] = height[0]; //make sure u initialise this first
-    
-        for(int i =1;i<n;i++){
-            maxi = max(maxi,height[i]);
-            res[i] =maxi;
-        }   
-        return res;
-    }
-    vector<int> right(vector<int>& height){
-        int n = height.size();
-        vector<int>res(n);
-
-        res[n-1] = height[n-1];
-        int maxi = height[n-1]; //stuck here
-
-        for(int i =n-2;i>=0;i--){
-            maxi = max(maxi,height[i]);
-            res[i] =maxi;
-        } 
-        return res; 
-    }
-
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> leftlimit = left(height);
-        vector<int> rightlimit = right(height);
+        int left = 0;
+        int right = n-1;
+        int maxleft = 0;
+        int maxright = 0;
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            int limit = min(leftlimit[i], rightlimit[i]);
-            ans += max(0, limit - height[i]);
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=maxleft){
+                    maxleft = height[left];
+                }
+                else{
+                    ans += maxleft - height[left];
+                }
+                left++;
+            }
+            else{//height[right]>height[left]
+                if(height[right]>maxright){
+                    maxright = height[right];
+                }
+                else{
+                    ans += maxright - height[right];
+                }
+                right--;
+            }
         }
-
         return ans;
     }
 };
