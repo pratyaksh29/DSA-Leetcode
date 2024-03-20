3d474b631af76db6ed36bd209826bc3e42855beb
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-20 16:29:39 +0530

    Time: 16 ms (68.46%) | Memory: 20.3 MB (63.49%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,43 +1,50 @@
 class MinStack {
 public:
-    stack<int>st;
-    stack<int>ss; //support stack
+    /** initialize your data structure here. */
+    stack<long> s;
+    long me;
     MinStack() {
         
     }
     
-    void push(int val) {
-        st.push(val);
-        if(ss.size()==0 || ss.top()>=val){ //use >= not only >
-            ss.push(val);
+    void push(int x) {
+        if(s.empty()){
+            s.push(x);
+            me = x;
+        }
+        else{
+            if(x <= me){
+                // put 2*x - me into stack
+                s.push(2ll*x - me);
+                me = x;
+            }
+            else{
+                s.push(x);
+            }
         }
     }
     
     void pop() {
-        int ans = st.top();
-        st.pop();
-        if(ans == ss.top()){
-            ss.pop();
+        if(s.empty()) return;
+        // stack top can never be smaller than me, if it is then it indicates a flag which means before popping, you have to update me
+        if(s.top() < me){
+            // me = 2*me - s.top()
+            me = 2ll*me - s.top();
         }
+        s.pop();
     }
     
+    // critical part
     int top() {
-        return st.top();
+        if(s.empty()) return -1;
+        if(s.top() < me){
+            return me;
+        }
+        return s.top();
     }
     
     int getMin() {
-        if(ss.size()==0){
-            return -1;
-        }
-        return ss.top();
+        if(s.empty()) return -1;
+        return me;
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
