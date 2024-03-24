5243d930dc931df86441cf997fff4807cfffd6fc
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-03-24 21:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (94.61%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -3,7 +3,6 @@ public:
     void solve(vector<string>&ans,string &str,int n,int open, int close){ //open>close always
         if(n==open && n==close){
             ans.push_back(str);
-            return;
         }
         //open or close
 
@@ -12,7 +11,7 @@ public:
             solve(ans,str,n,open+1,close);
             str.pop_back();
         }
-        if(close<n && close<open){
+        if(close<open){
             str.push_back(')');
             solve(ans,str,n,open,close+1);
             str.pop_back();
