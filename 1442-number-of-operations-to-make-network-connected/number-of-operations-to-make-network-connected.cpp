d4ee204f4a7c9173248b71bf0b4bc653a78813de
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-04-15 17:24:59 +0530

    Time: 82 ms (70.41%) | Memory: 45.9 MB (54.68%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,63 @@
+class DisjointSet{ //union by size
+	vector<int>size,parent;
+public:
+	DisjointSet(int n){ //constructor
+		size.resize(n+1); //n+1 if 1 based graph
+		parent.resize(n+1);
+		for(int i =0;i<=n;i++){
+			parent[i]=i;
+			size[i]=1;
+		}
+	}
+	int findUPar(int node){
+		if(node==parent[node]){
+			return node;
+		}
+		return parent[node]=findUPar(parent[node]);
+	}
+
+	void unionBySize(int u,int v){
+		int ulp_u = findUPar(u);
+		int ulp_v = findUPar(v);
+		if(ulp_u==ulp_v) return; //belonging to same component
+		if(size[ulp_u]<size[ulp_v]){
+			parent[ulp_u]=ulp_v;
+			size[ulp_v]+=size[ulp_u];
+		}
+		else{
+			parent[ulp_v]=ulp_u;
+			size[ulp_u]+=size[ulp_v];
+		}
+	}
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(n>connections.size()+1){
+            return -1;
+        }
+        int V = connections.size();
+        DisjointSet ds(V);
+        //need to find the number of connected components
+        int extraedges = 0;
+        for(auto it : connections){
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUPar(u)==ds.findUPar(v)){
+                extraedges++;
+            }
+            else{
+                ds.unionBySize(u,v);
+            }
+        }
+        int c = 0;
+        for(int i =0;i<n;i++){
+            if(ds.findUPar(i)==i){
+                c++;
+            }
+        }
+
+
+        return c-1;
+    }
+};
