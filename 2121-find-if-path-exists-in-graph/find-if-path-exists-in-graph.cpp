2a465c6bda252cfa5124ba152aa3e51fad4d25ce
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-05-31 00:01:28 +0530

    Time: 801 ms (34.66%) | Memory: 313.4 MB (5.11%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>>graph(n); // initialise graph with n nodes
+        vector<vector<int>>graph(n);
         for(auto edge : edges){
-            graph[edge[0]].push_back(edge[1]); //twice since its bidirectional
+            graph[edge[0]].push_back(edge[1]);
             graph[edge[1]].push_back(edge[0]);
         }
-        queue<int>q;
-        q.push(source);
+        stack<int>st;
+        st.push(source);
         vector<int>visited(n,0);
         visited[source]=1;
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            if(node==destination){
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();
+            if(node == destination){
                 return true;
             }
-            for(int adjacent : graph[node]){
-                if(visited[adjacent]==0){
-                    visited[adjacent]=1;
-                    q.push(adjacent);
-                }
+            for(auto adj : graph[node]){
+                if(!visited[adj])
+                    st.push(adj);
+                    visited[adj]=1;
             }
         }
         return false;
+
     }
 };
