506c2d6530490fae858be7fbdd61896cfb683822
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-06-10 11:50:08 +0530

    Time: 2 ms (48.97%) | Memory: 7.1 MB (86.03%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+
+public:
+    int dp[46]={0};
+    int climbStairs(int n) {
+        
+        if(n==0 || n==1){
+            return 1;
+        }
+        if(dp[n]){
+            return dp[n];
+        }
+        return dp[n]=climbStairs(n-1) + climbStairs(n-2);
+    }
+};
