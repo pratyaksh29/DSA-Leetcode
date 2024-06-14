1a69b2cbd426a691cb3ee5cd65dc68cf3f6bb1db
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-06-14 23:07:10 +0530

    Time: 56 ms (44.24%) | Memory: 19.4 MB (49.58%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,47 +1,44 @@
 class Solution {
-private:
-    int f(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
-        int row = grid.size();
-        int col = grid[0].size();
-
-        if (dp[i][j1][j2] != -1) {
-            return dp[i][j1][j2];
+public:
+    int f(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(j1<0 || j2<0 || j1>=m || j2>=m){
+            return INT_MIN;
         }
-
-        if (j1 < 0 || j1 >= col || j2 < 0 || j2 >= col) {
-            return -1e9;
-        }
-        if (i == row - 1) {
-            if (j1 == j2) {
+        if(i==grid.size()-1){
+            if(j1==j2){
                 return grid[i][j1];
             }
-            else {
+            else{
                 return grid[i][j1] + grid[i][j2];
             }
         }
-
-        // Explore all paths that Alice and Bob can go together
-        int maxi = -1e9;
-        for (int dj1 = -1; dj1 <= 1; dj1++) {
-            for (int dj2 = -1; dj2 <= 1; dj2++) {
-                if (j1 + dj1 >= 0 && j1 + dj1 < col && j2 + dj2 >= 0 && j2 + dj2 < col) {
-                    if (j1 == j2) {
-                        maxi = max(maxi, grid[i][j1] + f(i + 1, j1 + dj1, j2 + dj2, grid, dp));
-                    }
-                    else {
-                        maxi = max(maxi, grid[i][j1] + grid[i][j2] + f(i + 1, j1 + dj1, j2 + dj2, grid, dp));
-                    }
+        if(dp[i][j1][j2] != -1){
+            return dp[i][j1][j2];
+        }
+        int ans=INT_MIN;
+        for(int dj1 = -1;dj1<=1;dj1++){
+            for(int dj2 = -1;dj2<=1;dj2++){
+                int value = 0;
+                if(j1==j2){
+                    value= grid[i][j1] + f(i+1,j1+dj1,j2+dj2,grid,dp);
                 }
+                else{
+                    value= grid[i][j1] + grid[i][j2] + f(i+1,j1+dj1,j2+dj2,grid,dp);
+                }
+                ans = max(ans,value);
+
             }
         }
-        return dp[i][j1][j2] = maxi;
-    }
+        return dp[i][j1][j2]=ans;
 
-public:
+    }
     int cherryPickup(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        vector<vector<vector<int>>> dp(row, vector<vector<int>>(col, vector<int>(col, -1)));
-        return f(0, 0, col - 1, grid, dp);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+        return f(0,0,m-1,grid,dp);
+
     }
 };
