ba767f1e8df2649efcb4b9e9580993ba9058bed4
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-06-19 22:28:55 +0530

    Time: 51 ms (23.09%) | Memory: 46.7 MB (39.19%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,52 +1,29 @@
 class Solution {
-private:
-//     int f(int ind,int target,vector<int>& coins,vector<vector<int>>&dp){
-//         if(ind == 0 ){ 
-//             if(target%coins[0]==0){ //stuck here too ; if coins[0] are divisible by target -> return the total number it can be divided
-//                 return target/coins[0];
-//             }
-//             else{
-//                 return 1e9;
-//             }
-//         }
-//         if(dp[ind][target]!=-1){
-//             return dp[ind][target];
-//         }
-//         int notTake = 0 + f(ind-1,target,coins,dp);
-//         int take = 1e9;
-//         if(coins[ind]<=target){
-//             take= 1 + f(ind,target-coins[ind],coins,dp); // got stuck here -> stay on the same index till u have to change necessarily 
-//         }
-//         return dp[ind][target]=min(take,notTake);
-
-// //infinite supply, multiple use, etc -> whenever u take -> stand at same index
-//     }
-public: //tabulation
+public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int>base(amount+1,0),curr(amount+1,0);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
 
-        for(int i =0;i<=amount;i++){ //stuck here
+        for(int i =0;i<=amount;i++){
             if(i%coins[0]==0){
-                base[i]= i/coins[0];
+                dp[0][i]=i/coins[0];
             }
             else{
-                base[i]=1e9;
+                dp[0][i] = 1e9;
             }
         }
-        for(int i =1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                int dontpick = 0 + base[j];
-                int pick = 1e9;
-                if(coins[i]<=j){
-                    pick = 1+ curr[j-coins[i]];
 
+        for(int ind=1;ind<n;ind++){
+            for(int amoun = 0; amoun<=amount;amoun++){
+                int dontpick = dp[ind-1][amoun];
+                int pick = 1e9;
+                if(amoun-coins[ind]>=0){
+                    pick = 1+dp[ind][amoun-coins[ind]]; //dont go ind-1 as it can repeat
                 }
-                curr[j]=min(pick,dontpick);
+                dp[ind][amoun] = min(pick,dontpick);
             }
-            base=curr;
         }
-        int ans = base[amount];
+        int ans = dp[n-1][amount];
         if(ans>=1e9){
             return -1;
         }
