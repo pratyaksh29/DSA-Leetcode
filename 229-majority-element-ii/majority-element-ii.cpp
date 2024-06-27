f9fd5d6928d4b0c4e8a900ffb434df73b25ff261
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-06-28 00:21:08 +0530

    Time: 8 ms (66.06%) | Memory: 19.6 MB (16.97%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,52 +1,22 @@
-
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int ct1=0;
-        int ct2=0;
-        int el1 =INT_MIN;
-        int el2 = INT_MIN;
+        vector<int> result;
+        unordered_map<int, int> counts;//num and frequency
         
-        for(int i =0;i<n;i++){
-            if(ct1==0 && el2!=nums[i]){
-                el1=nums[i];
-                ct1=1;
-            }
-            else if(ct2==0 && el1!=nums[i]){
-                el2=nums[i];
-                ct2=1;
-            }
-            else if (nums[i]==el1){
-                ct1++;
-            }
-            else if (nums[i]==el2){
-                ct2++;
-            }
-            else{
-                ct1--;
-                ct2--;
+        // Count the occurrences of each element
+        for (int num : nums) {
+            counts[num]++;
+        }
+        
+        // Find the elements that appear more than n/3 times
+        for (auto it : counts) {
+            if (it.second > n / 3) {
+                result.push_back(it.first);
             }
         }
-        vector<int>ls;
-        ct1=0;
-        ct2=0;
-        for(int i =0;i<n;i++){
-            if(nums[i]==el1){
-                ct1++;
-            }
-            if(nums[i]==el2){
-                ct2++;
-            }
-        }
-        if(ct1>n/3){
-            ls.push_back(el1);
-        }
-        if(ct2>n/3){
-            ls.push_back(el2);
-        }
-        return ls;
-
-
+        
+        return result;
     }
 };
