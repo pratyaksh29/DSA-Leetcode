f46024123dfd3eb2c2ad488e88a117da10c07fbe
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-02 12:49:42 +0530

    Time: 2223 ms (5.02%) | Memory: 57.6 MB (98.37%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>minheap(nums.begin(),nums.begin()+k);
+    int findKthLargest(std::vector<int>& nums, int k) {
+        return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
+    }
 
-        for(int i =k;i<nums.size();i++){
-            if(nums[i]>minheap.top()){
-                minheap.pop();
-                minheap.push(nums[i]);
+private:
+    int quickSelect(std::vector<int>& nums, int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int pivotIndex = left + rand() % (right - left + 1);
+        pivotIndex = partition(nums, left, right, pivotIndex);
+
+        if (k == pivotIndex) {
+            return nums[k];
+        } else if (k < pivotIndex) {
+            return quickSelect(nums, left, pivotIndex - 1, k);
+        } else {
+            return quickSelect(nums, pivotIndex + 1, right, k);
+        }
+    }
+
+    int partition(std::vector<int>& nums, int left, int right, int pivotIndex) {
+        int pivotValue = nums[pivotIndex];
+        std::swap(nums[pivotIndex], nums[right]);  // Move pivot to end
+        int storeIndex = left;
+
+        for (int i = left; i < right; ++i) {
+            if (nums[i] < pivotValue) {
+                std::swap(nums[storeIndex], nums[i]);
+                storeIndex++;
             }
         }
-        return minheap.top();
+
+        std::swap(nums[right], nums[storeIndex]);  // Move pivot to its final place
+        return storeIndex;
     }
 };
