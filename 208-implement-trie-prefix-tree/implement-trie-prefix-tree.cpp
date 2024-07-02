2b021d1cba88ce5b54dc05f7ccbaa643aed590db
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-02 22:16:16 +0530

    Time: 52 ms (46.22%) | Memory: 48.3 MB (33.50%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,67 @@
+struct Node {
+    Node* links[26] = {nullptr};
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != nullptr;
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                return false;
+            }
+            node = node->get(ch);
+        }
+        return node->isEnd();
+    }
+
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for (char ch : prefix) {
+            if (!node->containsKey(ch)) {
+                return false;
+            }
+            node = node->get(ch);
+        }
+        return true;
+    }
+};
