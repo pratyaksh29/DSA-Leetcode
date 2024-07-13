aba62da425f08f9b8020210d5606c7c41bd99316
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-13 23:02:51 +0530

    Time: 17 ms (25.61%) | Memory: 16.6 MB (7.85%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,25 +1,26 @@
 class Solution {
-private:
-    int f(int ind1, int ind2, string &text1, string &text2,vector<vector<int>> &dp){//pass by reference as everytime u call the string, it might give tle
-        if(ind1<0 || ind2<0){
+public:
+    int f(int i, int j, string &word1, string &word2,vector<vector<int>>&dp){
+        if(i<0 || j<0){
             return 0;
         }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        if(text1[ind1]==text2[ind2]){
-            return dp[ind1][ind2] = 1 + f(ind1-1, ind2-1, text1,text2,dp);
+        if(word1[i]==word2[j]){
+            return dp[i][j]=1+f(i-1,j-1,word1,word2,dp);
         }
+        return dp[i][j] = max(f(i,j-1,word1,word2,dp),f(i-1,j,word1,word2,dp));
 
-        return dp[ind1][ind2] = max(f(ind1-1, ind2,text1,text2,dp),f(ind1, ind2-1,text1,text2,dp));
-    } 
-public:
+
+    }
     int minDistance(string word1, string word2) {
-        int m=word1.size();
-        int n=word2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        int a = f(word1.size()-1,word2.size()-1,word1,word2,dp);
-        int dels = (m-a) + (n-a);
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int a = f(n-1,m-1,word1,word2,dp);
+        int dels = (n-a) + (m-a);
         return dels;
+
     }
 };
