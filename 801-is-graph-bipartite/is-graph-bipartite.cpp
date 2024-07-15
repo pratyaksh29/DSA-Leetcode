b331443dc99a971b35ff9af60c7738691a431643
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-15 15:10:00 +0530

    Time: 22 ms (27.48%) | Memory: 16.4 MB (28.54%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
+    bool dfs(int a , int currcol , vector<vector<int>>& graph,vector<int>& color){
+        color[a] = !currcol;
+        for(auto it : graph[a]){
+            if(color[it]==-1){
+                if(dfs(it,!currcol,graph,color)==false){
+                    return false;
+                }
+            }
+            else if(color[a] == color[it]){
+                return false;
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int>color(n,0);//0 means uncolored
-
+        vector<int>color(n,-1);
         for(int i =0;i<n;i++){
-            if(color[i]!=0){
-                continue;
-            }
-            color[i]=1;
-            queue<int>q;
-            q.push(i);
-            while(!q.empty()){
-                auto node = q.front();
-                q.pop();
-                for(auto it : graph[node]){
-                    if(color[it]==0){
-                        color[it]= -color[node];
-                        q.push(it);
-                    }
-                    else if (color[it]==color[node]){
-                        return false;
-                    }
+            if(color[i]==-1){
+                if(dfs(i,0,graph,color)==false){
+                    return false;
                 }
             }
         }
         return true;
+
     }
 };
