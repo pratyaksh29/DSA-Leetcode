94521cfe8efbefafc4db99998a5075d15dcb17ca
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-19 13:28:13 +0530

    Time: 26 ms (31.00%) | Memory: 60.7 MB (18.50%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,39 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    TreeNode* first; // use this as temp
-    TreeNode* last;
-    TreeNode* prev;
+    TreeNode* first;
     TreeNode* middle;
-
-    void inorder(TreeNode* root) {
-        if (root == NULL) {
+    TreeNode* second;
+    TreeNode* prev;
+public:
+    void inorder(TreeNode* root){
+        if(root == NULL){
             return;
         }
-
         inorder(root->left);
-
-        if (prev != NULL && root->val < prev->val) { //MIDDLE PART IN INORDER TRAVERSAL
-            if (first == NULL) { //if it is the first violation
+        if(root->val < prev->val){
+            if(first==NULL){
                 first = prev;
                 middle = root;
-            } else { //if it is the second violation
-                last = root;
+            }
+            else{
+                second = root;
             }
         }
-        prev = root; //make prev equal to root because u r gonna go to the next branch
-
+        prev = root;
+        
         inorder(root->right);
     }
-
-public:
     void recoverTree(TreeNode* root) {
-
+        second = first = middle = NULL;
+        prev = new TreeNode(INT_MIN);
         inorder(root);
-
-        if (first && last) {
-            swap(first->val, last->val);
-        } else if (first && middle) {
-            swap(first->val, middle->val);
+        if(first&&second){
+            swap (first->val,second->val);
+        }
+        else if(first && middle){
+            swap(first->val,middle->val);
         }
     }
 };
