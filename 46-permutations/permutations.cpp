8fe90d4d19d4f1099393f74469cc7ce4261e6931
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-23 16:26:09 +0530

    Time: 2 ms (61.90%) | Memory: 9.1 MB (59.76%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
 
-    void solve(vector<int>& temp,vector<vector<int>>&ans, vector<int>&nums,int freq[]){
-        if(temp.size()==nums.size()){
-            ans.push_back(temp);
+    void solve(int ind, vector<vector<int>>&ans, vector<int>&nums){
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                temp.push_back(nums[i]);
-                freq[i]=1;
-                solve(temp,ans,nums,freq);
-                temp.pop_back();
-                freq[i]=0;
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            solve(ind+1,ans,nums);
+            swap(nums[ind],nums[i]);
+
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>temp;
-        int n =nums.size();
-        int freq[n];
-        for(int i=0;i<n;i++){
-            freq[i]=0;
-        }
-        solve(temp,ans,nums,freq);
+        solve(0,ans,nums);
         return ans;
 
     }
