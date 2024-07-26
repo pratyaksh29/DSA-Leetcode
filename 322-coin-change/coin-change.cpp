9fadc91c106f30ed714e0394d6b775a0f9cd9b3c
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-26 18:51:03 +0530

    Time: 48 ms (31.06%) | Memory: 51.7 MB (10.94%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-
-        for(int i =0;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i]=i/coins[0];
+    int f(int ind,int amount,vector<int>&coins,vector<vector<int>>&dp){
+        if(ind == 0){
+            if(amount%coins[0]==0){
+                return amount/coins[0];
             }
             else{
-                dp[0][i] = 1e9;
+                return 1e9;
             }
+        }
+        if(dp[ind][amount]!=-1){
+            return dp[ind][amount];
         }
 
-        for(int ind=1;ind<n;ind++){
-            for(int amoun = 0; amoun<=amount;amoun++){
-                int dontpick = dp[ind-1][amoun];
-                int pick = 1e9;
-                if(amoun-coins[ind]>=0){
-                    pick = 1+dp[ind][amoun-coins[ind]]; //dont go ind-1 as it can repeat
-                }
-                dp[ind][amoun] = min(pick,dontpick);
-            }
+        int dontpick = f(ind-1,amount,coins,dp);
+        int pick = 1e9;
+        if(amount-coins[ind]>=0){
+            pick = 1+ f(ind,amount-coins[ind],coins,dp);
         }
-        int ans = dp[n-1][amount];
-        if(ans>=1e9){
+        return dp[ind][amount] = min(pick,dontpick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        int ans = f(n-1,amount,coins,dp);
+        if(ans >=1e9){
             return -1;
         }
-        return ans;
+        else{
+            return ans;
+        }
     }
 };
