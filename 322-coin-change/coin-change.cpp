b39eccdd99ba800239d65a234c9a0e7488f2f972
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-26 18:57:06 +0530

    Time: 46 ms (35.09%) | Memory: 51.3 MB (11.98%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    int f(int ind,int amount,vector<int>&coins,vector<vector<int>>&dp){
+    int f(int ind,int amountt,vector<int>&coins,vector<vector<int>>&dp){
         if(ind == 0){
-            if(amount%coins[0]==0){
-                return amount/coins[0];
+            if(amountt%coins[0]==0){
+                return amountt/coins[0];
             }
             else{
                 return 1e9;
             }
         }
-        if(dp[ind][amount]!=-1){
-            return dp[ind][amount];
+        if(dp[ind][amountt]!=-1){
+            return dp[ind][amountt];
         }
 
-        int dontpick = f(ind-1,amount,coins,dp);
+        int dontpick = f(ind-1,amountt,coins,dp);
         int pick = 1e9;
-        if(amount-coins[ind]>=0){
-            pick = 1+ f(ind,amount-coins[ind],coins,dp);
+        if(amountt-coins[ind]>=0){
+            pick = 1+ f(ind,amountt-coins[ind],coins,dp);
         }
-        return dp[ind][amount] = min(pick,dontpick);
+        return dp[ind][amountt] = min(pick,dontpick);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans = f(n-1,amount,coins,dp);
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,1e9));
+
+        for(int i =0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]= i/coins[0];
+            }
+            else{
+                dp[0][i]=1e9;
+            }
+        }
+
+        for(int ind=1;ind<n;ind++){
+            for(int amountt =0;amountt<=amount;amountt++){
+                int dontpick = dp[ind-1][amountt];
+                int pick = 1e9;
+                if(amountt-coins[ind]>=0){
+                    pick = 1+ dp[ind][amountt-coins[ind]];
+                }
+                dp[ind][amountt] = min(pick,dontpick);
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans >=1e9){
             return -1;
         }
