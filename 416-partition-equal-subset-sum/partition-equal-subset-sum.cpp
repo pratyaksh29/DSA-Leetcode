f1c31c611f2530094be7bca626a9e4368fc13a95
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-26 12:08:09 +0530

    Time: 268 ms (32.20%) | Memory: 13 MB (89.16%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,49 +1,41 @@
 class Solution {
-private:
-    bool subsetSum(int ind, int target,vector<int>& nums){
-        vector<bool>base(target+1,0);
+public:
+    bool subsetsum(int ind, int k, vector<int>& nums){
+        vector<bool>base(k+1,false);
+        vector<bool>curr(k+1,false);
         base[0]=true;
-        if (nums[0] <= target){
+        curr[0]=true;
+        if(nums[0]==k){
             base[nums[0]]=true;
         }
         
-        for(int i =1;i<ind;i++){
-            vector<bool>curr(target+1,0);
-            curr[0]=true;
-            for(int j = 1; j<=target;j++){
-                bool notTake = base[j];
-                bool take = false;
-                if(nums[i]<=j){
-                    take = base[j-nums[i]];
+        
+
+        for(int i=1;i<nums.size();i++){
+            for(int j=1;j<=k;j++){
+                bool notpick = base[j];
+                bool pick = false;
+                if(j-nums[i]>=0){
+                    pick = base[j-nums[i]];
                 }
-                curr[j]=take || notTake;
+                curr[j]= pick || notpick;
             }
             base = curr;
         }
-        return base[target];
+        return base[k];
     }
-public:
-
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum=0;
-        for(int i =0 ; i<n;i++){
-            sum+=nums[i];
+        int target =0;
+        for(int i =0;i<nums.size();i++){
+            target += nums[i];
         }
-
-        if(sum%2==0){
-            int reqSum = sum/2;
-            return subsetSum(n,reqSum,nums);
+        if(target%2==0){
+            int sum = target/2;
+            return subsetsum(nums.size()-1,sum,nums);
         }
         else{
             return false;
         }
-
-        
-
- 
-
-        
         
     }
 };
