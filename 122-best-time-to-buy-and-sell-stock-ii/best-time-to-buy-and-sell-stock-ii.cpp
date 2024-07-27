13d8787401cec25a7d376316b654f877ea950ac7
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-27 21:44:23 +0530

    Time: 4 ms (71.93%) | Memory: 15.9 MB (39.62%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,43 +1,26 @@
 class Solution {
-// private: //memoization
-//     int f(int ind,int buy,vector<int>& prices,int n,vector<vector<int>>&dp){
-//         if(ind == n){
+public:
+//     int f(int ind,int buy,vector<int>& prices,vector<vector<int>>&dp){
+//         if(ind == prices.size()){
 //             return 0;
 //         }
-//         if(dp[ind][buy]!=-1){
-//             return dp[ind][buy];
-//         }
 //         int profit = 0;
-//         if(buy){ //buy -> pick or not pick
-//             profit = max(-prices[ind] + f(ind+1,0,prices,n,dp) , f(ind+1,1,prices,n,dp));
+//         if(buy){
+//             profit = max(-prices[ind],)
 //         }
-//         else{ //sell -> pick or not pick
-//             profit= max(prices[ind]+f(ind+1,1,prices,n,dp),f(ind+1,0,prices,n,dp));
-//         }
-//         return dp[ind][buy] = profit;
 //     }
-public:
     int maxProfit(vector<int>& prices) {
-        int n =prices.size();
-        vector<int>base(n+1,0),cur(n+1,0);
-
-        cur[0]=0;
-        cur[1]=0;
-
-
-        for(int ind =n-1;ind>=0;ind--){ //make sure u run the loop backwards 
-            for(int buy=0;buy<=1;buy++){
-                int profit = 0;
-                if(buy){ //buy -> pick or not pick
-                    profit = max(-prices[ind] + cur[0] , cur[1]);
-                }
-                else{ //sell -> pick or not pick
-                    profit = max(prices[ind] + cur[1] , cur[0]);
-                }
-                base[buy]=profit;
+        // int n = prices.size();
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return f(0,1,prices,dp);
+        int max_profit = 0;
+        for(int i =1;i< prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                max_profit += prices[i]-prices[i-1];
             }
-            cur = base;
         }
-        return cur[1];
+        return max_profit;
+
+        
     }
 };
